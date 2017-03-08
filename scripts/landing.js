5634b97cff18bc2cd176c2f640f76053d408d295
Andrea <apmansur1993@gmail.com>
2017-03-07 22:56:20 -0600

    refactor landing,js

--- scripts/landing.js
+++ scripts/landing.js
@@ -6,25 +6,22 @@
                      i.style.msTransform = "scaleX(1) translateY(0)";
                      i.style.WebkitTransform = "scaleX(1) translateY(0)";   
                 };
-
+ 
  var animatePoints = function(points) {
-     forEach(points, revealPoint);                       
-};
-    
+        forEach(points, revealPoint);
+                     };
 
  window.onload = function() {
-     
-     if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
+      if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
      }
      
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    
-     window.addEventListener('scroll', function(event) {
-          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray); 
-          }
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener("scroll", function(event) {
+             if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+             }
      });
  }
