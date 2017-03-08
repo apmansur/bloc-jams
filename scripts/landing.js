312bd83d238320b2054adb9bd5031b5bb943e97f
Andrea <apmansur1993@gmail.com>
2017-03-07 22:46:38 -0600

    refactor landing.js add utilities

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,30 @@
- var animatePoints = function() {
- 
-                var points = document.getElementsByClassName('point');
-                
- 
-                var revealPoint = function (i) {
-                     points[i].style.opacity = 1;
-                     points[i].style.transform = "scaleX(1) translateY(0)";
-                     points[i].style.msTransform = "scaleX(1) translateY(0)";
-                     points[i].style.WebkitTransform = "scaleX(1) translateY(0)";   
-                }
-                     for (var k=0; k<points.length; k++){
-                       revealPoint(k);
-                     }
- 
+ var pointsArray = document.getElementsByClassName('point');
+
+  var revealPoint = function (i) {
+                     i.style.opacity = 1;
+                     i.style.transform = "scaleX(1) translateY(0)";
+                     i.style.msTransform = "scaleX(1) translateY(0)";
+                     i.style.WebkitTransform = "scaleX(1) translateY(0)";   
+                };
+
+ var animatePoints = function(points) {
+     forEach(points, revealPoint);                       
 };
+    
+
+ window.onload = function() {
+     
+     if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+     }
+     
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+     window.addEventListener('scroll', function(event) {
+          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray); 
+          }
+     });
+ }
