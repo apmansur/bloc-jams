90731b38c189a7d1b1ad71fd015513cb8f577c97
Andrea <apmansur1993@gmail.com>
2017-03-22 19:54:13 -0500

    add template for albums.html

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,11 +1,11 @@
  var pointsArray = document.getElementsByClassName('point');
 
-  var revealPoint = function (i) {
+ var revealPoint = function (i) {
                      i.style.opacity = 1;
                      i.style.transform = "scaleX(1) translateY(0)";
                      i.style.msTransform = "scaleX(1) translateY(0)";
                      i.style.WebkitTransform = "scaleX(1) translateY(0)";   
-                };
+                }; 
  
  var animatePoints = function(points) {
         forEach(points, revealPoint);
